565837350646213a726c3948f69c81ebd25f6dfa
dnguy <dnguy008@gmail.com>
2015-05-25 21:43:40 -0700

    added ajax and finished services and about page

---
+++ assets/main.js
@@ -0,0 +1,34 @@
+function loadpage(page){
+	$.ajax({
+		url:'assets/pages/'+page+'.html',
+		dataType:'html',
+		cache: false,
+		success: function(response){
+			$('#main_content').html(response);
+		}
+	})
+}
+
+$(document).ready(function(){
+	$('.home_link').click(function(){
+		loadpage('home');
+	})
+	$('.contact_link').click(function(){
+		loadpage('contact');
+	})
+	$('.about_link').click(function(){
+		loadpage('about');
+	})
+	$('.services_link').click(function(){
+		loadpage('services');
+	})
+
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+
